8eecde205630269e9cb676199628386f2cac34f2
Bruno Galiz de Oliveira <brunogaliz@hotmail.com>
2020-06-07 03:03:49 -0400

    modified .gitignore

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import PointsController from './controller/PointsController';
 import ItemsController from './controller/ItemsController';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express();
 const upload = multer(multerConfig);
@@ -15,7 +16,24 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post('/points', 
+    upload.single('image'), 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            cidade: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+);
 
 
 export default routes;
